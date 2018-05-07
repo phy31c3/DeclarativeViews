6aea8d4f1e03f6cb9fc81c8ced3288224a36cc88
JongsunYoo <phy31c3@gmail.com>
2018-05-08 01:01:29 +0900

    인터페이스 작성

--- declarativeviews/src/main/java/kr/co/plasticcity/declarativeviews/DRVBuilderImpl.java
+++ declarativeviews/src/main/java/kr/co/plasticcity/declarativeviews/DRVBuilderImpl.java
@@ -19,7 +19,7 @@ import kr.co.plasticcity.declarativeviews.function.TriConsumer;
  * Created by JongsunYu on 2017-01-03.
  */
 
-class DRVBuilderImpl implements DRVBuilder.Buildable
+class DRVBuilderImpl implements DRVBuilder.Definable
 {
 	@NonNull
 	private final Consumer<DRVAdapter> applier;
@@ -33,78 +33,78 @@ class DRVBuilderImpl implements DRVBuilder.Buildable
 	}
 	
 	@Override
-	public <M> SingleGroupAdder<M, View> addGroup(@Nullable M model, int layoutResId)
+	public <M> SingleGroupAdder<M, View, Definable> addGroup(@Nullable M model, int layoutResId)
 	{
 		return addGroup(model, layoutResId, View.class);
 	}
 	
 	@Override
-	public <M, V> SingleGroupAdder<M, V> addGroup(@Nullable final M model, final int layoutResId, @NonNull final Class<V> viewType)
+	public <M, V> SingleGroupAdder<M, V, Definable> addGroup(@Nullable final M model, final int layoutResId, @NonNull final Class<V> viewType)
 	{
-		return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model), adapter, layoutResId, viewType));
+		return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model), adapter, layoutResId, viewType), this);
 	}
 	
 	@Override
-	public <M, V> SingleGroupAdder<M, V> addGroup(@Nullable final M model, @NonNull final Supplier<V> supplier)
+	public <M, V> SingleGroupAdder<M, V, Definable> addGroup(@Nullable final M model, @NonNull final Supplier<V> supplier)
 	{
-		return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model), adapter, supplier));
+		return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model), adapter, supplier), this);
 	}
 	
 	@Override
-	public <M> SingleGroupAdder<M, View> addGroup(@NonNull final SingleModel<M> model, final int layoutResId)
+	public <M> SingleGroupAdder<M, View, Definable> addGroup(@NonNull final SingleModel<M> model, final int layoutResId)
 	{
 		return addGroup(model, layoutResId, View.class);
 	}
 	
 	@Override
-	public <M, V> SingleGroupAdder<M, V> addGroup(@NonNull final SingleModel<M> model, final int layoutResId, @NonNull final Class<V> viewType)
+	public <M, V> SingleGroupAdder<M, V, Definable> addGroup(@NonNull final SingleModel<M> model, final int layoutResId, @NonNull final Class<V> viewType)
 	{
 		if (model instanceof DRVModel)
 		{
 			final DRVModel<M> m = (DRVModel<M>)model;
 			final DRVGroup<M, V> group = new DRVGroup<>(m, adapter, layoutResId, viewType);
 			m.init(group, group);
-			return new GroupAdderImpl<>(group);
+			return new GroupAdderImpl<>(group, this);
 		}
 		else
 		{
 			Log.w("DeclarativeRecyclerView", "Using an abnormal SingleModel. Use 'SingleModel.of(M m)' of 'SingleModel.empty()'");
-			return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model.get()), adapter, layoutResId, viewType));
+			return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model.get()), adapter, layoutResId, viewType), this);
 		}
 	}
 	
 	@Override
-	public <M, V> SingleGroupAdder<M, V> addGroup(@NonNull final SingleModel<M> model, @NonNull final Supplier<V> supplier)
+	public <M, V> SingleGroupAdder<M, V, Definable> addGroup(@NonNull final SingleModel<M> model, @NonNull final Supplier<V> supplier)
 	{
 		if (model instanceof DRVModel)
 		{
 			final DRVModel<M> m = (DRVModel<M>)model;
 			final DRVGroup<M, V> group = new DRVGroup<>(m, adapter, supplier);
 			m.init(group, group);
-			return new GroupAdderImpl<>(group);
+			return new GroupAdderImpl<>(group, this);
 		}
 		else
 		{
 			Log.w("DeclarativeRecyclerView", "Using an abnormal SingleModel. Use 'SingleModel.of(M m)' of 'SingleModel.empty()'");
-			return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model.get()), adapter, supplier));
+			return new GroupAdderImpl<>(new DRVGroup<>(Collections.singletonList(model.get()), adapter, supplier), this);
 		}
 	}
 	
 	@Override
-	public <M> GroupAdder<M, View> addGroup(@NonNull List<M> model, int layoutResId)
+	public <M> GroupAdder<M, View, Definable> addGroup(@NonNull List<M> model, int layoutResId)
 	{
 		return addGroup(model, layoutResId, View.class);
 	}
 	
 	@Override
-	public <M, V> GroupAdder<M, V> addGroup(@NonNull final List<M> model, final int layoutResId, @NonNull final Class<V> viewType)
+	public <M, V> GroupAdder<M, V, Definable> addGroup(@NonNull final List<M> model, final int layoutResId, @NonNull final Class<V> viewType)
 	{
 		if (model instanceof DRVModel)
 		{
 			final DRVModel<M> m = (DRVModel<M>)model;
 			final DRVGroup<M, V> group = new DRVGroup<>(m, adapter, layoutResId, viewType);
 			m.init(group, group);
-			return new GroupAdderImpl<>(group);
+			return new GroupAdderImpl<>(group, this);
 		}
 		else
 		{
@@ -112,19 +112,19 @@ class DRVBuilderImpl implements DRVBuilder.Buildable
 			{
 				Log.w("DeclarativeRecyclerView", "Using an abnormal ListModel. Use 'ListModel.of(Collection<M> m)' or 'ListModel.empty()'");
 			}
-			return new GroupAdderImpl<>(new DRVGroup<>(model, adapter, layoutResId, viewType));
+			return new GroupAdderImpl<>(new DRVGroup<>(model, adapter, layoutResId, viewType), this);
 		}
 	}
 	
 	@Override
-	public <M, V> GroupAdder<M, V> addGroup(@NonNull final List<M> model, @NonNull final Supplier<V> supplier)
+	public <M, V> GroupAdder<M, V, Definable> addGroup(@NonNull final List<M> model, @NonNull final Supplier<V> supplier)
 	{
 		if (model instanceof DRVModel)
 		{
 			final DRVModel<M> m = (DRVModel<M>)model;
 			final DRVGroup<M, V> group = new DRVGroup<>(m, adapter, supplier);
 			m.init(group, group);
-			return new GroupAdderImpl<>(group);
+			return new GroupAdderImpl<>(group, this);
 		}
 		else
 		{
@@ -132,8 +132,50 @@ class DRVBuilderImpl implements DRVBuilder.Buildable
 			{
 				Log.w("DeclarativeRecyclerView", "Using an abnormal ListModel. Use 'ListModel.of(Collection<M> m)' or 'ListModel.empty()'");
 			}
-			return new GroupAdderImpl<>(new DRVGroup<>(model, adapter, supplier));
+			return new GroupAdderImpl<>(new DRVGroup<>(model, adapter, supplier), this);
 		}
+	}
+	
+	@Override
+	public <M> SingleGroupAdder<M, View, Buildable> addFooter(@NonNull final M model, final int layoutResId)
+	{
+		// TODO: 2018-05-07
+		return null;
+	}
+	
+	@Override
+	public <M, V> SingleGroupAdder<M, V, Buildable> addFooter(@NonNull final M model, final int layoutResId, @NonNull final Class<V> viewType)
+	{
+		// TODO: 2018-05-07
+		return null;
+	}
+	
+	@Override
+	public <M, V> SingleGroupAdder<M, V, Buildable> addFooter(@NonNull final M model, @NonNull final Supplier<V> supplier)
+	{
+		// TODO: 2018-05-07
+		return null;
+	}
+	
+	@Override
+	public <M> SingleGroupAdder<M, View, Buildable> addFooter(@NonNull final SingleModel<M> model, final int layoutResId)
+	{
+		// TODO: 2018-05-07
+		return null;
+	}
+	
+	@Override
+	public <M, V> SingleGroupAdder<M, V, Buildable> addFooter(@NonNull final SingleModel<M> model, final int layoutResId, @NonNull final Class<V> viewType)
+	{
+		// TODO: 2018-05-07
+		return null;
+	}
+	
+	@Override
+	public <M, V> SingleGroupAdder<M, V, Buildable> addFooter(@NonNull final SingleModel<M> model, @NonNull final Supplier<V> supplier)
+	{
+		// TODO: 2018-05-07
+		return null;
 	}
 	
 	@Override
@@ -158,56 +200,59 @@ class DRVBuilderImpl implements DRVBuilder.Buildable
 		});
 	}
 	
-	private class GroupAdderImpl<M, V> implements SingleGroupAdder<M, V>
+	private class GroupAdderImpl<M, V, R> implements SingleGroupAdder<M, V, R>
 	{
 		@NonNull
 		private final DRVGroup<M, V> group;
+		@NonNull
+		private final R r;
 		
-		private GroupAdderImpl(@NonNull final DRVGroup<M, V> group)
+		private GroupAdderImpl(@NonNull final DRVGroup<M, V> group, @NonNull final R r)
 		{
 			this.group = group;
+			this.r = r;
 		}
 		
 		@Override
-		public SingleGroupAdder<M, V> onCreate(@NonNull final Consumer<V> func)
+		public SingleGroupAdder<M, V, R> onCreate(@NonNull final Consumer<V> func)
 		{
 			group.setOnCreate(func);
 			return this;
 		}
 		
 		@Override
-		public GroupAdder<M, V> onFistBind(@NonNull final BiConsumer<V, M> func)
+		public GroupAdder<M, V, R> onFistBind(@NonNull final BiConsumer<V, M> func)
 		{
 			group.setOnFirstBind((v, m, position) -> func.accept(v, m));
 			return this;
 		}
 		
 		@Override
-		public GroupAdder<M, V> onFistBind(@NonNull final TriConsumer<V, M, ItemPosition> func)
+		public GroupAdder<M, V, R> onFistBind(@NonNull final TriConsumer<V, M, ItemPosition> func)
 		{
 			group.setOnFirstBind(func);
 			return this;
 		}
 		
 		@Override
-		public GroupAdder<M, V> onBind(@NonNull final BiConsumer<V, M> func)
+		public GroupAdder<M, V, R> onBind(@NonNull final BiConsumer<V, M> func)
 		{
 			group.setOnBind((v, m, position) -> func.accept(v, m));
 			return this;
 		}
 		
 		@Override
-		public GroupAdder<M, V> onBind(@NonNull final TriConsumer<V, M, ItemPosition> func)
+		public GroupAdder<M, V, R> onBind(@NonNull final TriConsumer<V, M, ItemPosition> func)
 		{
 			group.setOnBind(func);
 			return this;
 		}
 		
 		@Override
-		public Buildable apply()
+		public R apply()
 		{
 			adapter.addGroup(group);
-			return DRVBuilderImpl.this;
+			return r;
 		}
 	}
 }
